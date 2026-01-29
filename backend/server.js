ff780997329770c29e413ae29a151269f824dd53
Bacem Touil <186514619+Bacemtouil-ZB@users.noreply.github.com>
2026-01-29 14:08:20 +0100

    Initial commit: backend MediVIH

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import db from "./config/db.js"; // pour connecter PostgreSQL
+import db from "./src/config/db.js"; // pour connecter PostgreSQL
 
 dotenv.config();
 const app = express();
